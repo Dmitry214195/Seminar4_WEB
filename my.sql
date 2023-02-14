9b2210117cbc802537b9a8405d9e7050ade64013
Dmitry Artamonow <dmitri.artamonow@yandex.ru>
2023-02-14 20:53:01 +0500

    Удалили лишнее

--- my.sql
+++ my.sql
@@ -5,13 +5,10 @@ CREATE TABLE classmate (
   address VARCHAR(30)NOT NULL
 );
 
--- insert
 INSERT INTO classmate VALUES (0001, 'Ivan', '18', 'Москва');
 INSERT INTO classmate VALUES (0002, 'Petr', '9', 'Минск');
 INSERT INTO classmate VALUES (0003, 'Anna', '19', 'Омск');
 INSERT INTO classmate VALUES (0004, 'Lucy', '88', 'Москва');
 INSERT INTO classmate VALUES (0005, 'Poppy', '69', 'Самара');
 
-
--- fetch s
 SELECT name FROM classmate WHERE age BETWEEN 15 AND 30 AND address = 'Москва';
